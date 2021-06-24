c4bece74f515059b9cffb42b0cc04207081996ec
Roberto <rpgrca@gmail.com>
2021-06-24 11:19:14 -0300

    remove unused usings

--- CSharp/C2-Patterns-Portfolio-Exercise-WithAccountImplementation/Patterns-Portfolio-Exercise-WithAccountImplementation/AccountTransaction.cs
+++ CSharp/C2-Patterns-Portfolio-Exercise-WithAccountImplementation/Patterns-Portfolio-Exercise-WithAccountImplementation/AccountTransaction.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Patterns_Portfolio_Exercise_WithAccountImplementation
+﻿namespace Patterns_Portfolio_Exercise_WithAccountImplementation
 {
     interface AccountTransaction
     {
--- CSharp/C2-Patterns-Portfolio-Exercise-WithAccountImplementation/Patterns-Portfolio-Exercise-WithAccountImplementation/Portfolio.cs
+++ CSharp/C2-Patterns-Portfolio-Exercise-WithAccountImplementation/Patterns-Portfolio-Exercise-WithAccountImplementation/Portfolio.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Patterns_Portfolio_Exercise_WithAccountImplementation
 {
