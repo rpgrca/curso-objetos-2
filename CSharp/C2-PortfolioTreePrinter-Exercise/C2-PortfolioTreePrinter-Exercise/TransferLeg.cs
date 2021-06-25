c744d02265f2d2a4da41db9138001041db7f754c
Roberto <rpgrca@gmail.com>
2021-06-25 12:09:13 -0300

    reformat, consolidate solutions

--- CSharp/C2-PortfolioTreePrinter-Exercise/C2-PortfolioTreePrinter-Exercise/TransferLeg.cs
+++ CSharp/C2-PortfolioTreePrinter-Exercise/C2-PortfolioTreePrinter-Exercise/TransferLeg.cs
@@ -5,33 +5,34 @@
         Transfer transfer();
     }
 
-    internal class DepositLeg : Deposit, TransferLeg
+    internal class DepositLeg : TransferLeg
     {
         private readonly Transfer _transfer;
 
         public DepositLeg(Transfer transfer, ReceptiveAccount toAccount)
-            : base(transfer.value())
         {
             _transfer = transfer;
             toAccount.register(this);
         }
 
+        public double applyTo(double balance) => balance + value();
+
         public Transfer transfer() => _transfer;
+
+        public double value() => _transfer.value();
     }
 
     internal class WithdrawLeg : TransferLeg
     {
         private readonly Transfer _transfer;
-        private readonly Withdraw _withdraw;
 
         public WithdrawLeg(Transfer transfer, ReceptiveAccount fromAccount)
         {
             _transfer = transfer;
-            _withdraw = new Withdraw(transfer.value());
             fromAccount.register(this);
         }
 
-        public double applyTo(double balance) => _withdraw.applyTo(balance);
+        public double applyTo(double balance) => balance - value();
 
         public Transfer transfer() => _transfer;
 
