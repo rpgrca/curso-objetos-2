835e2d963de39b2bb99289a46ccd4f04923f333d
Roberto <rpgrca@gmail.com>
2021-07-02 10:21:20 -0300

    new exercise

---
+++ CSharp/C2-ElevatorConsole-Exercise/ElevatorConsole-Exercise/ElevatorControllerConsole.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ElevatorConsole_Exercise
+{
+    class ElevatorControllerConsole : CabinStateVisitor, CabinDoorStateVisitor
+    {
+	    private List<String> console;
+
+	    public ElevatorControllerConsole(ElevatorController elevatorController) {
+            console = new List<String>();
+	    }
+
+	    protected void cabinDoorStateChangedTo(CabinDoorState cabinDoorState) {
+		    cabinDoorState.accept(this);
+	    }
+
+	    protected void cabinStateChangedTo(CabinState cabinState) {
+		    cabinState.accept(this);
+	    }
+
+	    public IEnumerator<String> consoleReader() {
+		    return console.GetEnumerator();
+	    }
+
+	    public void visitCabinMoving(CabinMovingState cabinMovingState) {
+		    console.Add("Cabina Moviendose");
+	    }
+
+	    public void visitCabinStopped(CabinStoppedState cabinStoppedState) {
+		    console.Add("Cabina Detenida");
+	    }
+
+	    public void visitCabinWaitingPeople(CabinWaitingForPeopleState cabinWaitingForPeopleState) {
+		    console.Add("Cabina Esperando Gente");
+	    }
+
+	    public void visitCabinDoorClosing(CabinDoorClosingState cabinDoorClosingState) {
+		    console.Add("Puerta Cerrandose");
+	    }
+
+	    public void visitCabinDoorClosed(CabinDoorClosedState cabinDoorClosedState) {
+		    console.Add("Puerta Cerrada");
+	    }
+
+	    public void visitCabinDoorOpened(CabinDoorOpenedState cabinDoorOpenedState) {
+		    console.Add("Puerta Abierta");
+	    }
+
+	    public void visitCabinDoorOpening(CabinDoorOpeningState cabinDoorOpeningState) {
+		    console.Add("Puerta Abriendose");
+	    }
+    }
+}
---
+++ CSharp/C2-ElevatorConsole-Exercise/ElevatorConsole-Exercise/ElevatorControllerStatusView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ElevatorConsole_Exercise
+{
+    class ElevatorControllerStatusView: CabinStateVisitor,CabinDoorStateVisitor 
+    {
+	    private String m_cabinFieldModel;
+	    private String m_cabinDoorFieldModel;
+
+	    public ElevatorControllerStatusView(ElevatorController elevatorController) {
+	    }
+
+	    protected void cabinDoorStateChangedTo(CabinDoorState cabinDoorState) {
+		    cabinDoorState.accept(this);
+	    }
+
+	    protected void cabinStateChangedTo(CabinState cabinState) {
+		    cabinState.accept(this);
+	    }
+
+	    
+	    public void visitCabinDoorClosing(CabinDoorClosingState cabinDoorClosingState) {
+		    m_cabinDoorFieldModel = "Closing";
+	    }
+
+	    
+	    public void visitCabinDoorClosed(CabinDoorClosedState cabinDoorClosedState) {
+		    m_cabinDoorFieldModel = "Closed";
+	    }
+
+	    
+	    public void visitCabinDoorOpened(CabinDoorOpenedState cabinDoorOpenedState) {
+		    m_cabinDoorFieldModel = "Open";
+	    }
+
+	    
+	    public void visitCabinDoorOpening(CabinDoorOpeningState cabinDoorOpeningState) {
+		    m_cabinDoorFieldModel = "Opening";
+	    }
+
+	    
+	    public void visitCabinMoving(CabinMovingState cabinMovingState) {
+		    m_cabinFieldModel = "Moving";
+	    }
+
+	    
+	    public void visitCabinStopped(CabinStoppedState cabinStoppedState) {
+		    m_cabinFieldModel = "Stopped";
+	    }
+
+	    
+	    public void visitCabinWaitingPeople(CabinWaitingForPeopleState cabinWaitingForPeopleState) {
+		    m_cabinFieldModel = "Waiting People";
+	    }
+
+	    public String cabinFieldModel() {
+		    return m_cabinFieldModel;
+	    }
+
+	    public String cabinDoorFieldModel() {
+		    return m_cabinDoorFieldModel;
+	    }
+    }
+}
