807faba21a92e3cb0d901c3a0a63b3f96a2333b5
Roberto <rpgrca@gmail.com>
2021-07-07 00:58:57 -0300

    remove unused usings

--- CSharp/C2-ElevatorConsole-Exercise/ElevatorConsole-Exercise.Logic/CabinDoorStateVisitor.cs
+++ CSharp/C2-ElevatorConsole-Exercise/ElevatorConsole-Exercise.Logic/CabinDoorStateVisitor.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ElevatorConsole_Exercise.Logic
+﻿namespace ElevatorConsole_Exercise.Logic
 {
     public interface CabinDoorStateVisitor
     {
--- CSharp/C2-ElevatorConsole-Exercise/ElevatorConsole-Exercise.UnitTests/ElevatorControllerViewTest.cs
+++ CSharp/C2-ElevatorConsole-Exercise/ElevatorConsole-Exercise.UnitTests/ElevatorControllerViewTest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Xunit;
+﻿using Xunit;
 using ElevatorConsole_Exercise.Logic;
 
 namespace ElevatorConsole_Exercise.UnitTests
